dafc896410c87918c5190ffabaa0fe73ea32f704
MaykeBolt <maykesouza2805@gmail.com>
2024-03-04 21:09:02 -0300

    Tela da página inicial terminada.

--- resources/views/screens/mainpage.blade.php
+++ resources/views/screens/mainpage.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body class="bg-slate-200">
     <header class="flex flex-row bg-gradient-to-r from-blue-900 to-cyan-600 py-6">
-        <h1 class="bg-gradient-to-br from-blue-300 to-blue-500 text-transparent bg-clip-text font-roboto 
+        <h1 class="bg-gradient-to-br from-blue-300 to-blue-500 text-transparent bg-clip-text font-mono 
         text-2xl font-bold pl-3
         hover:from-cyan-300 hover:via-cyan-300 hover:to-cyan-300"><a href="/home">CODETATIO</a></h1>
         <div class="ml-5">
@@ -32,11 +32,33 @@
 
     <main class="mx-3 my-3">
             <h1 class="text-3xl text-center">Bem vindo a CODETATIO!</h1>
-            <p class="mt-3 font-roboto">
-              Nós da CODETATIO temos o objetivo de desenvolver Softwares de alta qualidade e dar aos nossos usuários
-              uma experiência satifastória em todos os nossos objetivos
+            <p class="mt-3 font-mono text-xl text-center">
+              O site ainda está em desenvolvimento, mas aproveite para ver alguns dos nossos serviços que temos a oferecer:
             </p>
-        
+            
+            <div class="grid grid-cols-3 mt-10 gap bg-gradient-to-r from-slate-300 via-gray-400 to-slate-300 rounded-2xl">
+
+                <div class="border-4 border-solid border-blue-900 row-span-1 my-3 mx-3 rounded-lg text-white
+                bg-gradient-to-r from-blue-900 to-cyan-600">
+                    <h3 class="font-bold text-xl text-center">BooPeep</h3>
+                    <img src="BooPeep.png" alt="" height="auto" width="90%" class="mx-auto my-4" />
+                    <p class="font-sans mx-1">O BooPeep é um aplicação de achados e perdidos com o intuito de ajudar pessoas a retornar seus bens em uma instituição.</p>
+                    <p class="font-sans mx-1 my-1">Essa previa do projeto para a Semana Tecnologica da ETEC utiliza React, MongoDB e Tailwind CSS como tecnologias de desenvolvimento.</p>
+                </div>
+
+                <div class="row-span-2 border-4 border-solid border-blue-900 my-3 mx-3 rounded-lg text-white
+                bg-gradient-to-r from-blue-900 to-cyan-600">
+                    <h3 class="font-bold text-xl text-center">Em breve...</h3>
+                    <p class="font-sans mx-1 my-4">Esse projeto virá em breve, quando nós não sabemos mas virá.</p>
+                </div>
+
+                <div class="row-span-2 border-4 border-solid border-blue-900 my-3 mx-3 rounded-lg text-white
+                bg-gradient-to-r from-blue-900 to-cyan-600">
+                    <h3 class="font-bold text-xl text-center">Em breve...</h3>
+                    <p class="font-sans mx-1 my-4">Esse projeto virá em breve, quando nós não sabemos mas virá.</p>
+                </div>
+
+            </div>
         <!--
         <p>IMPORTANTE: Baixe a extensão: 'Laravel Blade Snippets' do Winnie Lin para poder escrever o HTML no arquivo blade.php mais facilmente</p>
         <p>Além disso baixe a extensão 'Tailwind CSS IntelliSense' para poder ver tudo de classes que o Tailwind pode oferecer.</p>
