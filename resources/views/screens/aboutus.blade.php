0f0b082b67c8b24b86694f5d9402906690f9255c
MaykeBolt <maykesouza2805@gmail.com>
2024-03-06 21:17:01 -0300

    Falta trocar as imagens e conseguir a descrição do Jhonas.

--- resources/views/screens/aboutus.blade.php
+++ resources/views/screens/aboutus.blade.php
@@ -51,11 +51,14 @@
                         <h1 class="text-xl font-mono mb-1">- Mayke Souza -</h1>
                         <div class="flex flex-row">
                         <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
-                        <p class="mx-3 text-left">Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text.
-                        </p>
+                            <div class="flex-col">
+                                <p class="mx-3 text-left">Lider do Front-End. Tenho conhecimento básico nas linguagems Java, JavaScript, TypeScript e PHP. Me sinto confortável
+                                    mechendo no Front-End, especialmente com CSS puro e Tailwind CSS. Já fiz projetos utilizando Frameworks como React, Angular e React Native.
+                                </p>
+                                <p class="mx-3 mt-3 text-left">Fora da área de programação, eu já utilizei editores de vídeo e audio para fazer vídeos para o Youtube como hobby e 
+                                    jogo vídeo games no meu tempo livre.
+                                </p>
+                            </div>
                         </div>
                     </div>
                     
@@ -63,50 +66,46 @@
                         <h1 class="text-xl font-mono mb-1">- Luís Fellipe -</h1>
                         <div class="flex flex-row">
                         <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
-                        <p class="mx-3 text-left">Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text.
-                        </p>
+                            <div class="flex-col">
+                                <p class="mx-3 text-left">Desenvolvedor Web e Mobile. Eu possuo conhecimentos para desenvolvimento de páginas Web utilizando HTML, CSS, Tailwind
+                                    CSS, JavaScript, TypeScript junto de frameworks como React e Angular. Também para desenvolver aplicações mobile utilizando React Native com 
+                                    TypeScript e StyleSheets.
+                                </p>
+                                <p class="mx-3 mt-3 text-left">Fora da área de programação, eu já desmontei e concertei celulares e o meu próprio computador além de ser
+                                     jovem aprendiz.
+                                </p>
+                            </div>
                         </div>
                     </div>
                     
-                    <div class="border border-sky-400 mx-4 my-3 p-3 bg-sky-600">
+                    <div class="border border-sky-400 mx-4 my-3 p-3 bg-indigo-600">
                         <h1 class="text-xl font-mono mb-1">- Igor Rocha -</h1>
                         <div class="flex flex-row">
                         <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
-                        <p class="mx-3 text-left">Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text.
-                        </p>
+                        <div class="flex-col">
+                            <p class="mx-3 text-left">Full-Stack do grupo, Dev-Ops. Sei fazer Expo, React, Node.JS, HTML, CSS, JavaScript, Angular,
+                                PHP e MySQL.
+                            </p>
+                            <p class="mx-3 mt-3 text-left">Fora da área de programação, eu gosto de jogar bola e jogos eletrônicos.
+                            </p>
+                        </div>
                         </div>
                     </div>
                 </div>
                 
                 <div class="bg-red-800 border-2 border-red-600 rounded-md">
                     <h1 class="font-bold text-2xl text-red-200">Back-End</h1>
-
-                    <div class="border border-red-400 mx-4 my-3 p-3 bg-red-700">
-                        <h1 class="text-xl font-mono mb-1">- Jhonas Rodrigues -</h1>
-                        <div class="flex flex-row">
-                        <p class="mx-3 text-left">Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text.
-                        </p>
-                        <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
-                        </div>
-                    </div>
                         
                     <div class="border border-red-400 mx-4 my-3 p-3 bg-red-700">
                         <h1 class="text-xl font-mono mb-1">- Jonathan Dias -</h1>
                         <div class="flex flex-row">
-                        <p class="mx-3 text-left">Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text.
-                        </p>
+                            <div class="flex-col">
+                                <p class="mx-3 text-left">Lider do Back-End. Sou um desenvolvedor capaz de fazer aplicações E2E, utilizando tecnologias como Angular, Node.JS,
+                                    TypeScript, Firebase, MongoDB e etc. Eu sei que tenho muito a aprender, mas estou sempre disposto à ir atrás de todos os meios para me aprimorar.
+                                </p>
+                                <p class="mx-3 mt-3 text-left">Fora da área de programação, eu gosto de ler, tocar músicas em meu violão e cantar.
+                                </p>
+                            </div>
                         <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
                         </div>
                     </div>
@@ -114,11 +113,29 @@
                     <div class="border border-red-400 mx-4 my-3 p-3 bg-red-700">
                         <h1 class="text-xl font-mono mb-1">- João Victor -</h1>
                         <div class="flex flex-row">
-                        <p class="mx-3 text-left">Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text Bottom text
-                            Bottom text.
-                        </p>
+                            <div class="flex-col">
+                                <p class="mx-3 text-left">Desenvolvedor Back-End e Designer. Eu tenho noção de Firebase, MongoDB, MySQL, JavaScript, TypeScript e HTML. Também tenho
+                                    noção básica de PHP, Angular e noção intermediária de CSS. Além disso, fiz os designs de nossos projetos aqui na CODETATIO.
+                                </p>
+                                <p class="mx-3 mt-3 text-left">Fora da área de programação, Eu fazia nadação como hobby, gosto de ler e sou um apreciador de música em geral.
+                                </p>
+                            </div>
+                        <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
+                        </div>
+                    </div>
+
+                    <div class="border border-red-400 mx-4 my-3 p-3 bg-rose-700">
+                        <h1 class="text-xl font-mono mb-1">- Jhonas Rodrigues -</h1>
+                        <div class="flex flex-row">
+                            <div class="flex-col">
+                                <p class="mx-3 text-left">Gerente e Lider da CODETATIO. Lidero tanto o Front-End quanto o Back-End. Eu tenho conhecimentos referente a HTML, CSS,
+                                    JavaScript, TypeScript, PHP, sei utilizar Frameworks como React e React Native, também sei coisas referente ao MongoDB e Firebase além de fazer
+                                    os conceitos iniciais do projeto e o cronograma para todos os membros.
+                                </p>
+                                <p class="mx-3 mt-3 text-left">Fora da área de programação, eu já utilizei editores de vídeo e audio para fazer vídeos para o Youtube como hobby e 
+                                    jogo vídeo games no meu tempo livre.
+                                </p>
+                            </div>
                         <img src="riff.jpg" alt="" height="auto" width="30%" class="rounded-3xl">
                         </div>
                     </div>
